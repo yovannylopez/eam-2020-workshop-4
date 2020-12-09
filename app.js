66b65811cd008f7953400b287d6855c57e7c69bb
yovanny_lopez <ylopez@toolboxtve.com>
2020-12-08 19:51:57 -0500

    pug and nodemon install

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-  res.send('¡Hello World!');
+  res.send('¡Hello World ....!');
 });
 
 app.listen(port, () => {
