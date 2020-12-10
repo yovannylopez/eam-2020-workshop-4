a840933663f34ec03bcb5c750cb38f11d2ca007d
yovanny_lopez <ylopez@toolboxtve.com>
2020-12-09 21:34:01 -0500

    template views, call database, styles base

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const data = require('./database/data.json');
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('¡Hello World ....!');
+  res.render('index', {
+    data: data.albums
+  });
 });
 
 app.listen(port, () => {
